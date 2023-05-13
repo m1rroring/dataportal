e81638aad004d35e7726a916795a9a51fec9c6c1
inanepolo <inanepolo@sina.cn>
2023-05-13 10:51:38 +0800

    增加保存XML文件

--- src/views/catalog/catalog.vue
+++ src/views/catalog/catalog.vue
@@ -412,8 +412,23 @@
                   "recordSetStartPoint": 0,
                   "recordSetEndPoint": 1
               }
-          }).then(json => {
-              console.log(json);
+          }).then(res => {
+              console.log(res);
+              // const content = res;
+              const blob = new Blob([res]);
+              const fileName = id + '.xml';
+              if ('download' in document.createElement('a')) { // 非IE下载
+                  const elink = document.createElement('a');
+                  elink.download = fileName;
+                  elink.style.display = 'none';
+                  elink.href = URL.createObjectURL(blob);
+                  document.body.appendChild(elink);
+                  elink.click();
+                  URL.revokeObjectURL(elink.href) ; // 释放URL 对象
+                  document.body.removeChild(elink);
+              // } else { // IE10+下载
+              //     navigator.msSaveBlob(blob, fileName)
+              }
           });
       },
       async showTablePopup(datasetCode,datasetName) {
